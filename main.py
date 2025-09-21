6056118ac7c4decf7e90e4963530698cd6a1b4b6
TimCha99 <orkan7260@gmail.com>
2025-09-21 15:31:58 +0900

    로컬 main.py 수정 내용 커밋

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import picamera2
 app = Flask(__name__)
 
 picam2 = picamera2.Picamera2()
-picam2.configure(picam2.create_preview_configuration(main={"size": (1280, 720)}))
+picam2.configure(picam2.create_preview_configuration(main={"size": (640, 480)}))
 picam2.start()
 
 def generate_frames():
